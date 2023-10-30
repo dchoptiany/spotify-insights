5de9dbe29bf45b99d6a98b2b4f64eb4f59685478
Aleksandra <woks076@gmail.com>
2023-10-30 22:08:01 +0100

    Passport.js implemented

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const overviewRouter = require('./routers/overviewRouter')
 const cookieSession = require('cookie-session')
 const cors = require('cors')
+const passport = require('passport')
+const passportSetup = require("./passport")
+const auth = require("./routers/auth")
+const session = require('express-session')
+
 
 
 const app = express();
@@ -9,19 +14,28 @@ const app = express();
 //Middlewares
 app.use(express.json())
 app.use(cookieSession({
-    name : 'session',
+    name : 'a',
     keys: ['spotify-insights'],
     maxAge : 24*60*60*60
 }))
+app.use(
+    session({secret: 'spotify-insights', 
+    resave: true, 
+    saveUninitialized: true})
+  );
+
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 //Routers
-app.use('/overview', overviewRouter);
+//app.use('/overview', overviewRouter);
 app.use(cors({
     origin: "http://localhost:3000",
     methods: "GET,POST,PUT,DELETE",
     credentials : true
 }))
-
+app.use("/auth",auth)
 
 module.exports = app
