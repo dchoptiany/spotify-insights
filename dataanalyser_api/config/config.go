b7e2670971edb033385a1e44e67052cf13f3fb57
Aleksandra Zaremba <aleksandra.zaremba02@gmail.com>
2023-12-10 21:01:29 +0100

    Hotfix Data Analyser Makefile

--- dataanalyser_api/config/config.go
+++ dataanalyser_api/config/config.go
@@ -3,7 +3,7 @@ package config
 const (
 	SpotifyInsightsRootDir = "/home/ola/WORKSPACE/pwr/sem5/zespolowe/UI/spotify-insights"
 	SpotifyInsightsRuntime = SpotifyInsightsRootDir + "/spotify-insights/runtime"
-	DataAnalyserCliBinary  = SpotifyInsightsRuntime + "/da/bin/data_analyser"
+	DataAnalyserCliBinary  = SpotifyInsightsRuntime + "/da/bin/data_analyser-linux"
 	DataAnalyserTmpFiles   = SpotifyInsightsRuntime + "/da/tmp"
 
 	DatacollectorAddr                             = "http://localhost:8080"
