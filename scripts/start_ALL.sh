ea35dd807a854a232bc6d1ee063d2d800524bafb
Aleksandra Zaremba <aleksandra.zaremba02@gmail.com>
2023-12-11 23:49:01 +0100

    Add docker based deployment, UI code refactoring #2

---
+++ scripts/start_ALL.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./start_dataanalyser_api.sh
+./start_datacollector.sh
+./start_ui_frontend.sh
+./start_ui_passport_spotify.sh
--- scripts/start_ui_passport_spotify.sh
+++ scripts/start_ui_passport_spotify.sh
@@ -2,8 +2,7 @@
 PORT=${1:-8000}
 VERSION=${2:-latest}
 
-export SPOTIFY_CLIENT_ID='90ec6685e54d40a2aeb635c633191c1d'
-export SPOTIFY_CLIENT_SECRET='176eaea4634e4676b4f431c8b58a836d'
+. ./secrets.env
 
 docker run -d -p ${PORT}:${PORT} -e PORT=${PORT} \
 -e SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID} \
