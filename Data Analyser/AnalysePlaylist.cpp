39891ba3604dfa8318ce31b951f56031c942d1f5
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-12-13 13:20:48 +0100

    Added AnalysePlaylist.cpp

---
+++ Data Analyser/AnalysePlaylist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "DataAnalyser.hpp"
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        std::cerr << "Invalid arguments." << std::endl;
+        return 1;
+    }
+    std::string inputJson = argv[1];
+    
+    DataAnalyser* dataAnalyser = new DataAnalyser();
+    std::cout << dataAnalyser->analysePlaylist(inputJson) << std::endl;
+    delete dataAnalyser;
+    return 0;
+}
