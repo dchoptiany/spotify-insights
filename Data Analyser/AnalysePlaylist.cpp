295a0b65d7ff764392bf2d6eb704d7292fe52f4b
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-12-13 13:34:20 +0100

    Scripts improvements

--- Data Analyser/AnalysePlaylist.cpp
+++ Data Analyser/AnalysePlaylist.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "DataAnalyser.hpp"
 
+/*
+    Script to run playlist analysis. 
+    Requires JSON string containing playlist's tracklist as first parameter.
+    All other parameters will be ignored.
+    Returns results as a indented JSON to the standard output and returns 0 in case of success.
+    Sends 'Invalid arguments.' to the error output instead and returns 1 in case of no arguments.
+    Sends 'Invalid input.' to the error output instead and returns 1 in case of invalid data.
+*/
 int main(int argc, char* argv[])
 {
     if(argc < 2)
@@ -11,7 +19,17 @@ int main(int argc, char* argv[])
     std::string inputJson = argv[1];
     
     DataAnalyser* dataAnalyser = new DataAnalyser();
-    std::cout << dataAnalyser->analysePlaylist(inputJson) << std::endl;
+    try
+    {
+        std::cout << dataAnalyser->analysePlaylist(inputJson) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Invalid input." << std::endl;
+        delete dataAnalyser;
+        return 1;
+    }
+    
     delete dataAnalyser;
     return 0;
 }
--- Data Analyser/AnalyseProfile.cpp
+++ Data Analyser/AnalyseProfile.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "DataAnalyser.hpp"
 
+/*
+    Script to run user's music taste analysis. 
+    Requires JSON string containing user's liked tracks list as first parameter.
+    All other parameters will be ignored.
+    Sends results as a indented JSON to the standard output and returns 0 in case of success.
+    Sends 'Invalid arguments.' to the error output instead and returns 1 in case of no arguments.
+    Sends 'Invalid input.' to the error output instead and returns 1 in case of invalid data.
+*/
 int main(int argc, char* argv[])
 {
     if(argc < 2)
@@ -11,7 +19,17 @@ int main(int argc, char* argv[])
     std::string inputJson = argv[1];
     
     DataAnalyser* dataAnalyser = new DataAnalyser();
-    std::cout << dataAnalyser->analyseLikedTracks(inputJson) << std::endl;
+    try
+    {
+        std::cout << dataAnalyser->analyseLikedTracks(inputJson) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Invalid input." << std::endl;
+        delete dataAnalyser;
+        return 1;
+    }
+
     delete dataAnalyser;
     return 0;
 }
