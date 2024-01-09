f7caaa35c21bb1f2c1d58aad0b4488d767f2038c
woycik <woks07@wp.pl>
2024-01-09 19:01:17 +0100

    User info

--- ui_frontend/src/components/TextCart.js
+++ ui_frontend/src/components/TextCart.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card, Metric, Text } from "@tremor/react";
 
-const generateCard = ({ data, text }) => (
+const generateTextCard = ({ data, text }) => (
     <Card className="max-w-xs mx-auto" style={{ width: "30vh", margin:"0.7%" }} decoration="top" decorationColor="indigo">
     <Text>{text}</Text>
     <Text>{data}</Text>
   </Card>
 );
 
-export default generateCard;
+export default generateTextCard;
