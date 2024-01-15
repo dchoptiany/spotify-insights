47705a2b2b1a31454b681f26db0e401db4ef4690
woycik <woks07@wp.pl>
2024-01-15 23:21:42 +0100

    Data Sketches update

--- ui_frontend/src/pages/GlobalTrends.js
+++ ui_frontend/src/pages/GlobalTrends.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import generateLineChart from '../components/LineChart';
 import { Flex } from "@tremor/react";
 import Button from '../components/Button';
+import { DateRangePicker } from "@tremor/react";
 import  {DataSketchesRequest} from './../actions/authActions'
 
 
@@ -11,131 +12,76 @@ const GlobalTrends = () => {
     const [endDate, setEndDate] = useState("");
     const [display, setDisplay] = useState(false);
     const [url_data, setData] = useState({});
+    
 
   
-    const handleStartDateChange = (e) => {
-      const selectedDate = e.target.value;
-      const formattedDate = formatDate(selectedDate);
-      setStartDate(selectedDate);
-    };
-    
-    const handleEndDateChange = (e) => {
-      const selectedDate = e.target.value;
-      const formattedDate = formatDate(selectedDate);
-      setEndDate(selectedDate);
-    };
-    
-    const formatDate = (dateString) => {
-      const [year, month, day] = dateString.split("-");
-      return `${day}-${month}-${year}`;
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    };
-  
- let exampleData=
- {
-     "date_labels": [
-         "01-01-2024",
-         "08-01-2024",
-         "15-01-2024",
-         "22-01-2024",
-         "29-01-2024"
-     ],
-     "genre_scores": [
-         [
-             "pop",
-             [
-                 50,
-                 60,
-                 55,
-                 62,
-                 65
-             ]
-         ],
-         [
-             "rap",
-             [
-                 20,
-                 15,
-                 18,
-                 10,
-                 10
-             ]
-         ],
-         [
-             "disco",
-             [
-                 30,
-                 25,
-                 22,
-                 28,
-                 25
-             ]
-         ]
-     ]
- };
+let exampleData=[]
 
+const handleSubmit = ()=>{
+
+}
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}-${month}-${year}`;
+};
 
  const handleButtonClick =  () => {
-  setDisplay(true);
-  // try {
-  //     const parsedSpotifyURL = "http://aws_hostname:6060/data_sketch/trends";
-  //     DataSketchesRequest(parsedSpotifyURL, formatDate(startDate), formatDate(endDate))
-  //     .then(response => {
-  //       console.log(response)
-  //       return response.text()
-  //     }) 
-  //     .then(data => {
-  //       setDisplay(true);
-  //       const cleanedData = data.replace(/"/g, '');
-  //       console.log(data)
-  //       let requestData = JSON.parse(atob(cleanedData));
-  //       console.log(requestData)
- //
-  //       setData(requestData);
-  //       setDisplay(true);
- //
- //
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //       setDisplay(false);
-  //     });
-  // } catch (error) {
-  //   console.error('Błąd podczas zapytania:', error);
-  // }
+   try {
+       const parsedSpotifyURL = "http://aws_hostname:6060/data_sketch/trends";
+       DataSketchesRequest(parsedSpotifyURL, formatDate(startDate), formatDate(endDate))
+       .then(response => {
+         console.log(response)
+         return response.text()
+       }) 
+       .then(data => {
+         setDisplay(true);
+         const cleanedData = data.replace(/"/g, '');
+         console.log(data)
+         let requestData = JSON.parse(atob(cleanedData));
+         console.log(requestData)
+ 
+         setData(requestData);
+         setDisplay(true);
+ 
+ 
+       })
+       .catch(error => {
+         console.log(error);
+         setDisplay(false);
+       });
+   } catch (error) {
+     console.error('Błąd podczas zapytania:', error);
+   }
   };
+
+
+  const updateDates = ({start,end})=>{
+    setStartDate(start)
+    setEndDate(end)
+  }
 
   
  return (
     <div className="input-form-container">
 
-      <form onSubmit={handleSubmit} className='form'>
+      <form  className='form'>
       <Flex justifyContent="center" alignItems="center" width="50%">
-
-        <label className='element'>
-          Start Date:  .    
-          <input type="date" className='dateInput' value={startDate} onChange={handleStartDateChange} />
-        </label>
-        </Flex>
-        <br />
-        
-        <Flex justifyContent="center" alignItems="center" width="50%">
-
-        <label className='element'>
-          End Date: .  
-          <input type="date" value={endDate}  className='dateInput' onChange={handleEndDateChange} />
-        </label>
-        <br />
-        </Flex>
-
-        <Flex justifyContent="center" alignItems="center" width="50%">
-
+      <DateRangePicker 
+        className="datePicker" 
+        enableSelect={false} 
+        minDate={1705273200000} 
+        maxDate={new Date()}   
+        onValueChange={(selectedDates) => {updateDates({start: formatDate(selectedDates.from), end: formatDate(selectedDates.to)})}}>
+        </DateRangePicker>
+      </Flex>
+      <Flex justifyContent="center" alignItems="center" width="50%">
         <Button onClick={handleButtonClick} text="Submit" />
         </Flex>
-
       </form>
       
       <div className='plots'>
