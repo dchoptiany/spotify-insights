a8b0a33d4b2f7bf366dfb13a35d9cd11e005ec69
woycik <woks07@wp.pl>
2024-01-16 12:18:09 +0100

    bug

--- ui_frontend/src/components/LineChart.js
+++ ui_frontend/src/components/LineChart.js
@@ -5,12 +5,10 @@ import { Card, LineChart, Title } from "@tremor/react";
 function convertDataFormat(exampleData) {
     const result = [];
   
-    // Iteruj przez etykiety dat
     for (let i = 0; i < exampleData.date_labels.length; i++) {
       const dateLabel = exampleData.date_labels[i];
       const dataObj = { date_labels: dateLabel };
   
-      // Iteruj przez gatunki i przypisz odpowiednie wartości
       for (let j = 0; j < exampleData.genre_scores.length; j++) {
         const genre = exampleData.genre_scores[j][0];
         const score = exampleData.genre_scores[j][1][i];
@@ -26,7 +24,7 @@ function convertDataFormat(exampleData) {
 
 
   const generateLineChart = ({ data, text }) => {
-    // Konwertuj dane przed użyciem w wykresie
+    console.log(data)
     const musicTypes = data.genre_scores.map(([genre]) => genre);
     const newData = convertDataFormat(data);
 
@@ -34,7 +32,6 @@ function convertDataFormat(exampleData) {
     return (
       <Card className="lineChart">
         <Title>{text}</Title>
-        {/* Użyj nowo sformatowanych danych */}
         <LineChart
           className="mt-6"
           data={newData}
