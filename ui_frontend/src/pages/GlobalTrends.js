74c60146dc9495bf539f55a2f53770e01bc68a42
woycik <woks07@wp.pl>
2024-01-16 21:58:54 +0100

    Parsing date

--- ui_frontend/src/pages/GlobalTrends.js
+++ ui_frontend/src/pages/GlobalTrends.js
@@ -15,11 +15,15 @@ const GlobalTrends = () => {
     
 
 const formatDate = (dateString) => {
+  if (isNaN(Date.parse(dateString))) {
+    return "";
+  }
+
   const date = new Date(dateString);
   const day = date.getDate().toString().padStart(2, '0');
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
-  if(day == NaN){
+  if (isNaN(day)) {
     return "";
   }
 
