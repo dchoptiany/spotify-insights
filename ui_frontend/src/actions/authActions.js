5d8b398f77547b48d77bbd8a6a1dc056062a098b
woycik <woks07@wp.pl>
2024-01-16 11:55:35 +0100

    bug

--- ui_frontend/src/actions/authActions.js
+++ ui_frontend/src/actions/authActions.js
@@ -43,21 +43,22 @@ export const DataCollectorRequest = (endpoint) => {
 };
 
 
-export const DataSketchesRequest = (endpoint,startDate, endDate) => {  
-  
-  return fetch("http://aws_hostname:8000/api/dataSketches", {
-    method: 'GET',
-    credentials: "include",
-    headers: {
-      Accept: "application/json",
-      endpoint : endpoint,
-      startDate: startDate,
-      endDate: endDate,
-      'Content-Type': 'application/json',
-      "Access-Control-Allow-Credentials": true,
-    },
-  })
+export const DataSketchesRequest = (endpoint, startDate, endDate) => {  
+  const queryParams = new URLSearchParams({
+    endpoint: endpoint,
+    startdate: startDate,
+    enddate: endDate,
+  });
 
+  return fetch(`http://aws_hostname:8000/api/dataSketches?${queryParams}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
 };
+
 
 
