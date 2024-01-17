b9a0525aa93535d580d61f58d29f9e8bfde0962e
Aleksandra Zaremba <aleksandra.zaremba02@gmail.com>
2024-01-17 15:00:43 +0100

    Update app with new Spotify Dashboard

--- scripts/start_datacollector.sh
+++ scripts/start_datacollector.sh
@@ -3,6 +3,6 @@ PORT=${1:-8080}
 VERSION=${2:-latest}
 
 docker run -d -p ${PORT}:${PORT} --name datacollector \
-  -e SPOTIFY_CLIENT_ID='90ec6685e54d40a2aeb635c633191c1d' \
-  -e SPOTIFY_CLIENT_SECRET='176eaea4634e4676b4f431c8b58a836d' \
+  -e SPOTIFY_CLIENT_ID='d81422beff084f90a56a188b173fd47f' \
+  -e SPOTIFY_CLIENT_SECRET='d810c89a0da640439721ef6c572ea183' \
   spotify-insights-datacollector:${VERSION}
