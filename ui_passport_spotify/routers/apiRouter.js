52dae5fa953c7b56aefdf05a26b0f74ed9daca61
woycik <woks07@wp.pl>
2024-01-18 19:40:23 +0100

    Sending data

--- ui_passport_spotify/routers/apiRouter.js
+++ ui_passport_spotify/routers/apiRouter.js
@@ -85,7 +85,7 @@ router.get("/dataSketchesCombo", (req, res) => {
 
   dataCollectorAction(apiEndpoint, jsonQuery)
     .then((data) => {
-      res.status(200).send(jsonQuery);
+      res.status(200).send(data);
     })
     .catch((error) => {
       res.status(404).send(jsonQuery);
