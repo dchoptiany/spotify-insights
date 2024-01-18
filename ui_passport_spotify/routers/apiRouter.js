90077a58607f700b1d08edc64cdbcb28f48931fc
woycik <woks07@wp.pl>
2024-01-18 15:18:37 +0100

    Testing

--- ui_passport_spotify/routers/apiRouter.js
+++ ui_passport_spotify/routers/apiRouter.js
@@ -72,11 +72,14 @@ router.get("/dataSketchesCombo", (req, res) => {
   const endDate = req.query.enddate;
   const arrayJson = req.query.array;
 
+  const array = JSON.parse(arrayJson);
+  const arrayString  = JSON.stringify(array);
+
 
   const jsonQuery = JSON.stringify({
     "start_date": startDate,
     "end_date": endDate,
-    "data" : arrayJson
+    "data" : arrayString
   });
 
   dataCollectorAction(apiEndpoint, jsonQuery)
