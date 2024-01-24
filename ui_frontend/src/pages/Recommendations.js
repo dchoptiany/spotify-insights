f97cd7e85f7a5550b69186b4f8d0479c20b1541b
woycik <woks07@wp.pl>
2024-01-24 12:36:59 +0100

    fix

--- ui_frontend/src/pages/Recommendations.js
+++ ui_frontend/src/pages/Recommendations.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import generateTextCard from '../components/recommendationCart';
 import { Flex } from "@tremor/react";
 import {DataCollectorRequest} from '../actions/authActions';
