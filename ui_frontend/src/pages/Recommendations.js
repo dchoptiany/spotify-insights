e4e0a37cf88c46cb639a95b457c6522865efde2b
woycik <woks07@wp.pl>
2024-01-24 12:31:15 +0100

    abortController

--- ui_frontend/src/pages/Recommendations.js
+++ ui_frontend/src/pages/Recommendations.js
@@ -1,57 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import generateTextCard from '../components/recommendationCart';
 import { Flex } from "@tremor/react";
-import { DataCollectorRequest } from '../actions/authActions';
+import {DataCollectorRequest} from '../actions/authActions';
 import AudioPlayer from './AudioPlayer';
 
+
+//Recommendation page
 const Recommendations = () => {
   const [display, setDisplay] = useState(false);
   const [userData, setUserData] = useState({
     tracks: []
   });
 
-  let isMounted = true;
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const parsedSpotifyURL = "http://aws_hostname:8080/spotify-api/user/recommendations";
-        console.log(parsedSpotifyURL);
+  let requestData=""
 
-        const response = await DataCollectorRequest(parsedSpotifyURL);
-        const data = await response.text();
-        const requestData = JSON.parse(data);
-        if(isMounted){
-          setUserData(requestData);
-          setDisplay(true);
-        }
-        else{
-          isMounted=false;
-        }
-      } catch (error) {
-        console.error('Błąd podczas zapytania:', error);
-      }
-    };
 
-    getData(); 
+//Sending request and collecting recieved data
+useEffect(() => {
+  const getData = async () => {
+    try {
+      const abortController = new AbortController();
+      const parsedSpotifyURL = "http://aws_hostname:8080/spotify-api/user/recommendations";
+      console.log(parsedSpotifyURL);
 
-  }, []); 
+      const response = await DataCollectorRequest(parsedSpotifyURL);
+      const data = await response.text();
+      const requestData = JSON.parse(data);
+
+      setUserData(requestData);
+      setDisplay(true);
+    } catch (error) {
+      console.error('Błąd podczas zapytania:', error);
+    }
+  };
+
+  getData(); 
+
+  return () => {
+    abortController.abort()
+  }
+
+}, []);
+
 
   return (
     <div className="input-form-container">
-      <div style={{ paddingTop: "4%" }}>
-        {userData.tracks.map((artist, index) => (
-          <div className='recommendations' key={index}>
-            <Flex justifyContent="center" alignItems="center" className='cartRec'>
-              <img src={artist.album_image} alt={artist.name} style={{ width: '70px', height: '70px', marginRight: '2%' }} />
-              {display && generateTextCard(artist.name, artist.artist_name, artist.album_name)}
-            </Flex>
-            <Flex justifyContent="center" alignItems="center" className='cart'>
-              <AudioPlayer previewUrl={artist.preview_url} />
-            </Flex>
-          </div>
-        ))}
+        <div style={{paddingTop:"4%"}}>
+            {userData.tracks.map((artist, index) => (
+              <div className='recommendations' key={index}>
+                <Flex justifyContent="center" alignItems="center" className='cartRec' >
+                <img src={artist.album_image} alt={artist.name} style={{ width: '70px', height: '70px', marginRight: '2%' }} />
+                {display && generateTextCard(artist.name, artist.artist_name,artist.album_name)}
+
+                </Flex>
+                <Flex justifyContent="center" alignItems="center" className='cart' >
+
+                <AudioPlayer previewUrl={artist.preview_url} />
+                </Flex>
+
+
+              </div>
+            ))}
+            </div>
       </div>
-    </div>
   );
 };
 
